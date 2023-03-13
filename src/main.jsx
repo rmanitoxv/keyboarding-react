997d22ec3c48f96fc6aeedb53b24b5b8d9e46e0b
PC 05 <manito.russel@gmail.com>
2023-03-13 15:20:13 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+
+import Root from './routes/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/Root.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class Root extends PureComponent {
+  render() {
+    return (
+      <div>Hello World!!</div>
+    )
+  }
+}
+
+export default Root
