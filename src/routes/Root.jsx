e8795f2e92821752c7f607a5d910f79f35cbcc5e
PC 05 <manito.russel@gmail.com>
2023-03-13 17:02:39 +0800

    Created Sample  Homepage

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,17 @@
 import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Root extends PureComponent {
   render() {
     return (
-      <div>Hello World!!</div>
+    <div className='h-screen'>
+      <div className='flex flex-col h-full justify-center items-center text-[6rem] text-center'>
+        <p className='pb-12'>
+            Welcome to<br/>TECHNO TYPER 
+        </p>
+        <Link to={`#`}>START</Link> 
+      </div>
+    </div>
     )
   }
 }
